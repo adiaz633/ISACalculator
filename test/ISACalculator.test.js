473f73a359169cfd23352e10a722b3b4a0eba4fe
sebasvillalbagancedo <sebas.villalba@gmail.com>
2023-06-08 11:24:04 +0200

    Global testing modified by using Factory Method dessign pattern

--- test/ISACalculator.test.js
+++ test/ISACalculator.test.js
@@ -1,47 +1,54 @@
 import { ISACalculator } from "../src/ISACalculator";
+import { ISACalculatorFactory } from "../src/ISACalculatorFactory";
 
 
 describe("ISACalculator reference", () => {
   /**
    * @type {ISACalculator}
    */
-  let calculatorObj;
+  let calculatorObj; // Global glass for arithmetical operations
 
-  // Instantiate calculator object to perform unit tests
+  /**
+   * @type {ISACalculatorFactory}
+   */
+  let calculatorFactory; // Factory class to implement solution using Factory Method design pattern
+
+  // Instantiate calculator object and factory object to perform unit tests
   beforeEach(() => {
     calculatorObj = new ISACalculator();
+    calculatorFactory = new ISACalculatorFactory();
   });
 
   describe("Global TESTS for Any aritmetical Operation", () => {
-    // These tests check arimetical operations available
+    // These tests check arithmetical operations available
 
     test("Check addition operation available", () => {
-      const result = calculatorObj.setOperation("+");
+      const result = calculatorFactory.setOperation("+");
       expect(result).toBeTruthy();
     });
 
     test("Check substraction operation available", () => {
-      const result = calculatorObj.setOperation("-");
+      const result = calculatorFactory.setOperation("-");
       expect(result).toBeTruthy();
     });
 
     test("Check multiply operation available", () => {
-      const result = calculatorObj.setOperation("*");
+      const result = calculatorFactory.setOperation("*");
       expect(result).toBeTruthy();
     });
 
     test("Check division available", () => {
-      const result = calculatorObj.setOperation("/");
+      const result = calculatorFactory.setOperation("/");
       expect(result).toBeTruthy();
     });
 
     test("Check root square available", () => {
-      const result = calculatorObj.setOperation("sqrt");
+      const result = calculatorFactory.setOperation("sqrt");
       expect(result).toBeTruthy();
     });
 
     test("Aritmetical operation not available", () => {
-      const result = calculatorObj.setOperation("other");
+      const result = calculatorFactory.setOperation("other");
       expect(result).toBeFalsy();
     });
 
