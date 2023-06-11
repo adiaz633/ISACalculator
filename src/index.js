8ede208761199e78f66f9e4e775a89329bc13aeb
adiaz633 <alberto.diaz633@comunidadunir.net>
2023-06-11 21:31:45 +0200

    styles for messaging and implemented actions on buttons

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ require('./css/style.css');
 import { ISACalculatorFactory } from "./ISACalculatorFactory.js"
 
 let DOMMessage;
-
+let DOMparamsTextInput;
+let DOMoperationTextInput;
 class App {
   constructor() {
     this.calculatorFactory = new ISACalculatorFactory();
@@ -22,7 +23,24 @@ class App {
       numbers.push(numberConverted);
     });
     const result = this.calculatorFactory.makeOperation(operatorParam, ...numbers);
+    this.setResult(result);
+  }
+
+  setResult(result) {
+    if (isNaN(result)) {
+      DOMMessage.classList.add("input-error");
+    } else {
+      DOMMessage.classList.add("input-valid");
+    }
     DOMMessage.textContent = result;
+  }
+
+  resetValues() {
+    DOMparamsTextInput.value = "";
+    DOMoperationTextInput.value = "";
+    DOMMessage.textContent = "";
+    DOMMessage.classList.remove("input-error");
+    DOMMessage.classList.remove("input-valid");
   }
 
 
@@ -30,9 +48,11 @@ class App {
   start() {
     const DOMwrapper = document.querySelector(".formbold-form-wrapper");
     const DOMroot = document.getElementById("root");
-    const DOMparamsTextInput = document.getElementById("arguments");
-    const DOMoperationTextInput = document.getElementById("operation");
+    DOMparamsTextInput = document.getElementById("arguments");
+    DOMoperationTextInput = document.getElementById("operation");
     const DOMcalculateButton = document.getElementById("calculate");
+    const DOMresetButton = document.getElementById("reset");
+
     DOMMessage = document.getElementById("message");
     //Show Form
     DOMroot.classList.toggle("active");
@@ -42,6 +62,10 @@ class App {
     DOMcalculateButton.addEventListener("click", () => {
       this.evaluateElements(DOMparamsTextInput.value, DOMoperationTextInput.value);
     }, this);
+
+    DOMresetButton.addEventListener("click", () => {
+      this.resetValues();
+    },this);
   }
 }
 
